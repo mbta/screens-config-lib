242d77506c53414bf848decffd70cc0c0884916b
cmaddox5 <christian.maddox93@gmail.com>
2024-01-03 14:27:04 -0500

    Another fix.

--- lib/config/v2/gl_eink.ex
+++ lib/config/v2/gl_eink.ex
@@ -2,16 +2,7 @@ defmodule ScreensConfig.V2.GlEink do
   @moduledoc false
   # credo:disable-for-this-file Credo.Check.Design.DuplicatedCode
 
-  alias ScreensConfig.V2.{
-    Alerts,
-    Audio,
-    Departures,
-    EvergreenContentItem,
-    Footer,
-    LineMap,
-    PlatformLocation
-  }
-
+  alias ScreensConfig.V2.{Alerts, Audio, Departures, EvergreenContentItem, Footer, LineMap}
   alias ScreensConfig.V2.Header.Destination
 
   @type t :: %__MODULE__{
@@ -22,7 +13,7 @@ defmodule ScreensConfig.V2.GlEink do
           line_map: LineMap.t(),
           evergreen_content: list(EvergreenContentItem.t()),
           audio: Audio.t(),
-          platform_location: PlatformLocation
+          platform_location: :front | :back
         }
 
   @enforce_keys [:departures, :footer, :header, :alerts, :line_map]
@@ -43,7 +34,11 @@ defmodule ScreensConfig.V2.GlEink do
       alerts: Alerts,
       line_map: LineMap,
       evergreen_content: {:list, EvergreenContentItem},
-      audio: Audio,
-      platform_location: PlatformLocation
+      audio: Audio
     ]
+
+  defp value_from_json("platform_location", location),
+    do: location |> String.downcase() |> String.to_existing_atom()
+
+  defp value_to_json(_, value), do: value
 end
