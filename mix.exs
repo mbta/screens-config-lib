7a8f79d15417d64413f679f3948597489f0d394e
cmaddox5 <christian.maddox93@gmail.com>
2024-03-05 11:52:52 -0500

    Removed runtime arg.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ScreensConfig.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6.0", only: [:dev, :test]},
       {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
   end
