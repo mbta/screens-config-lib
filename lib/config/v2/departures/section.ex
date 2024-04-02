f5d84ae9fd1f6364a9d6896cd6f6f2d0b8cb6a7b
Cora Grant <cgrant2@mbta.com>
2024-04-02 17:28:07 -0400

    feat: add fields for V2 Busway screens

--- lib/config/v2/departures/section.ex
+++ lib/config/v2/departures/section.ex
@@ -1,22 +1,34 @@
 defmodule ScreensConfig.V2.Departures.Section do
-  @moduledoc false
+  @moduledoc """
+  Configures a section within the Departures widget. Sections are a means of grouping departures
+  by mode, stopping location, etc. Each section can fetch and display its departures differently.
 
-  alias ScreensConfig.V2.Departures.{Filter, Headway, Query}
+  - `bidirectional` enables a special mode that displays exactly two departures: the first one
+    that would normally be displayed, and the next departure on the same route in the opposite
+    direction.
+  """
+
+  alias ScreensConfig.V2.Departures.{Filters, Header, Headway, Layout, Query}
 
   @type t :: %__MODULE__{
           query: Query.t(),
-          filter: Filter.t() | nil,
+          filters: Filters.t(),
+          header: Header.t(),
           headway: Headway.t(),
-          bidirectional: boolean() | nil
+          layout: Layout.t(),
+          bidirectional: boolean()
         }
 
   @enforce_keys [:query]
   defstruct query: nil,
-            filter: nil,
+            filters: Filters.from_json(:default),
+            header: Header.from_json(:default),
             headway: Headway.from_json(:default),
+            layout: Layout.from_json(:default),
             bidirectional: false
 
-  use ScreensConfig.Struct, children: [query: Query, filter: Filter, headway: Headway]
+  use ScreensConfig.Struct,
+    children: [query: Query, header: Header, filters: Filters, headway: Headway]
 
   defp value_from_json(_, value), do: value
 
