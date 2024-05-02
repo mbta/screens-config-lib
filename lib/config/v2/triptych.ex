0084cb873d2bb892cfa42cb6a4f62caf7410e857
cmaddox5 <christian.maddox93@gmail.com>
2024-05-02 13:45:24 -0400

    Testing something.

--- lib/config/v2/triptych.ex
+++ lib/config/v2/triptych.ex
@@ -2,25 +2,29 @@ defmodule ScreensConfig.V2.Triptych do
   @moduledoc false
 
   alias ScreensConfig.V2.{EvergreenContentItem, LocalEvergreenSet, TrainCrowding}
+  alias ScreensConfig.V2.Header.CurrentStopId
 
   @type t :: %__MODULE__{
           train_crowding: TrainCrowding.t(),
           local_evergreen_sets: list(LocalEvergreenSet.t()),
           evergreen_content: list(EvergreenContentItem.t()),
-          show_identifiers: boolean()
+          show_identifiers: boolean(),
+          reconstructed_alert_widget: CurrentStopId.t()
         }
 
   @enforce_keys [:train_crowding, :local_evergreen_sets]
   defstruct train_crowding: nil,
             local_evergreen_sets: [],
             evergreen_content: [],
-            show_identifiers: false
+            show_identifiers: false,
+            reconstructed_alert_widget: nil
 
   use ScreensConfig.Struct,
     children: [
       train_crowding: TrainCrowding,
       local_evergreen_sets: {:list, LocalEvergreenSet},
-      evergreen_content: {:list, EvergreenContentItem}
+      evergreen_content: {:list, EvergreenContentItem},
+      reconstructed_alert_widget: CurrentStopId
     ]
 
   defp value_from_json(_, value), do: value
