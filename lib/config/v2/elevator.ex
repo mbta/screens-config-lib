febe11b3c221303549ba3b15ba0a1046e5f9d94a
cmaddox5 <christian.maddox93@gmail.com>
2024-11-20 13:40:41 -0500

    Added additional fields for alternate directions on elevators.

--- lib/config/v2/elevator.ex
+++ lib/config/v2/elevator.ex
@@ -3,14 +3,23 @@ defmodule ScreensConfig.V2.Elevator do
 
   alias ScreensConfig.V2.EvergreenContentItem
 
+  @type arrow_direction :: :n | :s | :e | :w
+
   @type t :: %__MODULE__{
           elevator_id: String.t(),
-          evergreen_content: list(EvergreenContentItem.t())
+          evergreen_content: list(EvergreenContentItem.t()),
+          alternate_direction_text: String.t(),
+          accessible_path_image_url: String.t(),
+          accessible_path_direction_arrow: arrow_direction()
         }
 
-  @enforce_keys [:elevator_id]
-  defstruct elevator_id: nil,
-            evergreen_content: []
+  @enforce_keys [
+    :elevator_id,
+    :alternate_direction_text,
+    :accessible_path_image_url,
+    :accessible_path_direction_arrow
+  ]
+  defstruct @enforce_keys ++ [evergreen_content: []]
 
   use ScreensConfig.Struct,
     children: [
