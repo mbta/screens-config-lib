bbc9575e375b28b0489732eefa66c2ca3f100d0e
Christian Maddox <christian.maddox93@gmail.com>
2024-11-25 09:04:00 -0500

    update: Elevator config (#45)
    * Config updates.
    
    * Address PR feedback.

--- lib/config/v2/elevator.ex
+++ lib/config/v2/elevator.ex
@@ -8,17 +8,22 @@ defmodule ScreensConfig.V2.Elevator do
           elevator_id: String.t(),
           evergreen_content: list(EvergreenContentItem.t()),
           alternate_direction_text: String.t(),
-          accessible_path_image_url: String.t(),
-          accessible_path_direction_arrow: Arrow.t()
+          accessible_path_image_url: String.t() | nil,
+          accessible_path_direction_arrow: Arrow.t(),
+          accessible_path_image_here_coordinates: %{x: non_neg_integer(), y: non_neg_integer()}
         }
 
   @enforce_keys [
     :elevator_id,
     :alternate_direction_text,
-    :accessible_path_image_url,
     :accessible_path_direction_arrow
   ]
-  defstruct @enforce_keys ++ [evergreen_content: []]
+  defstruct @enforce_keys ++
+              [
+                evergreen_content: [],
+                accessible_path_image_url: nil,
+                accessible_path_image_here_coordinates: %{x: 0, y: 0}
+              ]
 
   use ScreensConfig.Struct,
     children: [
