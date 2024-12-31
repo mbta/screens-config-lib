47dc31b3aab01027bf885b154bf6253b3d134ef3
Kim <pkim@mbta.com>
2024-12-31 11:56:13 -0500

    Use uturn to be more descriptive

--- lib/config/arrow.ex
+++ lib/config/arrow.ex
@@ -3,5 +3,5 @@ defmodule ScreensConfig.Arrow do
   Represents an arrow direction to be displayed on screens.
   """
 
-  @type t :: :n | :ne | :e | :se | :s | :sw | :w | :nw | :u | nil
+  @type t :: :n | :ne | :e | :se | :s | :sw | :w | :nw | :uturn | nil
 end
