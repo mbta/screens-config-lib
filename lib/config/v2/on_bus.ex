ecbd4eda51f0653a23c69084b898cf28b27489c1
robbie.sundstrom <rsundstrom@mbta.com>
2025-01-30 14:58:01 -0500

    remove header reference

--- lib/config/v2/on_bus.ex
+++ lib/config/v2/on_bus.ex
@@ -1,13 +1,9 @@
 defmodule ScreensConfig.V2.OnBus do
   @moduledoc false
 
-  alias ScreensConfig.V2.Header.CurrentStopId
-
   @type t :: %__MODULE__{}
 
   @enforce_keys []
-
-  defstruct header: nil
 
   use ScreensConfig.Struct,
     children: []
