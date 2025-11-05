5982c53de7a5aaff7087d47b1e874b42bbeaec63
Robbie Sundstrom <rsundstrom@mbta.com>
2025-11-05 15:40:51 -0500

    chore: Remove `Opts` and `include_schedules` from Departures Query config (#71)

--- lib/config/departures/query.ex
+++ lib/config/departures/query.ex
@@ -1,15 +1,13 @@
 defmodule ScreensConfig.Departures.Query do
   @moduledoc false
 
-  alias ScreensConfig.Departures.Query.{Opts, Params}
+  alias ScreensConfig.Departures.Query.Params
 
   @type t :: %__MODULE__{
-          params: Params.t(),
-          opts: Opts.t()
+          params: Params.t()
         }
 
-  defstruct params: Params.from_json(:default),
-            opts: Opts.from_json(:default)
+  defstruct params: Params.from_json(:default)
 
-  use ScreensConfig.Struct, with_default: true, children: [params: Params, opts: Opts]
+  use ScreensConfig.Struct, with_default: true, children: [params: Params]
 end
