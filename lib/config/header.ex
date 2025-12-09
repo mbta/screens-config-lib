9bec8f941dd47d2baffcafab342bbf4a1d1b371a
robbie.sundstrom <rsundstrom@mbta.com>
2025-12-09 18:56:17 -0500

    Add read_as field to header config

--- lib/config/header.ex
+++ lib/config/header.ex
@@ -3,6 +3,9 @@ defmodule ScreensConfig.Header do
   Common header type for most screens. Can be configured with a `stop_name` (really an arbitrary
   string, presented as-is) or a `stop_id` (indicating the header should be the name of that stop).
 
+  `read_as` is how the section should be announced in audio readouts. If `nil`, defaults to the
+    configured `title`.
+
   `use ScreensConfig.Header` inserts appropriate `value_from_json` and `value_to_json` clauses to
   encode and decode these variants.
   """
@@ -10,10 +13,9 @@ defmodule ScreensConfig.Header do
   defmodule StopId do
     @moduledoc false
 
-    @type t :: %__MODULE__{stop_id: String.t()}
+    @type t :: %__MODULE__{stop_id: String.t() | nil, read_as: String.t() | nil}
 
-    @enforce_keys [:stop_id]
-    defstruct @enforce_keys
+    defstruct [:stop_id, :read_as]
 
     use ScreensConfig.Struct
 
@@ -24,10 +26,9 @@ defmodule ScreensConfig.Header do
   defmodule StopName do
     @moduledoc false
 
-    @type t :: %__MODULE__{stop_name: String.t()}
+    @type t :: %__MODULE__{stop_name: String.t() | nil, read_as: String.t() | nil}
 
-    @enforce_keys [:stop_name]
-    defstruct @enforce_keys
+    defstruct [:stop_name, :read_as]
 
     use ScreensConfig.Struct
 
@@ -41,6 +42,7 @@ defmodule ScreensConfig.Header do
     quote location: :keep do
       defp value_from_json("header", %{"stop_name" => _} = value), do: StopName.from_json(value)
       defp value_from_json("header", %{"stop_id" => _} = value), do: StopId.from_json(value)
+      defp value_from_json("header", %{"read_as" => _} = value), do: StopName.from_json(value)
       defp value_to_json(:header, %module{} = value), do: module.to_json(value)
     end
   end
