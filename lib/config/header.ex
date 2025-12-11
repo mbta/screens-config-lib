e14b7f68b8c1530c69281a0ce4df3d6718a11986
robbie.sundstrom <rsundstrom@mbta.com>
2025-12-11 13:09:46 -0500

    Make stop_id and stop_name required

--- lib/config/header.ex
+++ lib/config/header.ex
@@ -3,8 +3,8 @@ defmodule ScreensConfig.Header do
   Common header type for most screens. Can be configured with a `stop_name` (really an arbitrary
   string, presented as-is) or a `stop_id` (indicating the header should be the name of that stop).
 
-  `read_as` is how the section should be announced in audio readouts. If `nil`, defaults to the
-    configured `title`.
+  `read_as` is how the screen should be announced in audio readouts: "This is [read_as]".
+   If `nil`, defaults to the stop_name or the name inferred from the stop_id
 
   `use ScreensConfig.Header` inserts appropriate `value_from_json` and `value_to_json` clauses to
   encode and decode these variants.
@@ -13,9 +13,10 @@ defmodule ScreensConfig.Header do
   defmodule StopId do
     @moduledoc false
 
-    @type t :: %__MODULE__{stop_id: String.t() | nil, read_as: String.t() | nil}
+    @type t :: %__MODULE__{stop_id: String.t(), read_as: String.t() | nil}
 
-    defstruct [:stop_id, :read_as]
+    @enforce_keys [:stop_id]
+    defstruct @enforce_keys ++ [read_as: nil]
 
     use ScreensConfig.Struct
 
@@ -26,9 +27,10 @@ defmodule ScreensConfig.Header do
   defmodule StopName do
     @moduledoc false
 
-    @type t :: %__MODULE__{stop_name: String.t() | nil, read_as: String.t() | nil}
+    @type t :: %__MODULE__{stop_name: String.t(), read_as: String.t() | nil}
 
-    defstruct [:stop_name, :read_as]
+    @enforce_keys [:stop_name]
+    defstruct @enforce_keys ++ [read_as: nil]
 
     use ScreensConfig.Struct
 
@@ -42,7 +44,6 @@ defmodule ScreensConfig.Header do
     quote location: :keep do
       defp value_from_json("header", %{"stop_name" => _} = value), do: StopName.from_json(value)
       defp value_from_json("header", %{"stop_id" => _} = value), do: StopId.from_json(value)
-      defp value_from_json("header", %{"read_as" => _} = value), do: StopName.from_json(value)
       defp value_to_json(:header, %module{} = value), do: module.to_json(value)
     end
   end
