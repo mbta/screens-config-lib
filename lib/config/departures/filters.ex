e9ba3de3859d65fa4ede83e9a58a6635954d1d1d
Dean Shi <dshi@mbta.com>
2026-08-04 09:51:01 -0400

    refactor: remove max_minutes option from Departure Filters

--- lib/config/departures/filters.ex
+++ lib/config/departures/filters.ex
@@ -2,18 +2,16 @@ defmodule ScreensConfig.Departures.Filters do
   @moduledoc """
   Configures filtering for the departures displayed in a `Section`.
 
-  - `max_minutes` limits how far in the future departures are allowed to be.
   - `route_directions` limits departures based on specific combinations of route and direction.
   """
 
   alias ScreensConfig.Departures.Filters.RouteDirections
 
   @type t :: %__MODULE__{
-          max_minutes: non_neg_integer() | nil,
           route_directions: RouteDirections.t() | nil
         }
 
-  defstruct max_minutes: nil, route_directions: nil
+  defstruct route_directions: nil
 
   use ScreensConfig.Struct, children: [route_directions: RouteDirections], with_default: true
 
